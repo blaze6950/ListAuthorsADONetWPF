ff14f0523a8aa9457142023b17e9dff1479b8fd4
blaze6950 <d.progect21@gmail.com>
2018-05-09 02:00:19 +0300

    Update: - added all the functionality

--- ListAuthorsADONetWPF/MainWindow.xaml.cs
+++ ListAuthorsADONetWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,267 @@ namespace ListAuthorsADONetWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ListView myListView;
         public MainWindow()
         {
             InitializeComponent();
+            myListView = new ListView();
+
+            GridView myGridView = new GridView();
+            myGridView.AllowsColumnReorder = true;
+            myGridView.ColumnHeaderToolTip = "Authors Information";
+
+            GridViewColumn gvc1 = new GridViewColumn();
+            gvc1.DisplayMemberBinding = new Binding("FirstName");
+            gvc1.Header = "FirstName";
+            gvc1.Width = 100;
+            myGridView.Columns.Add(gvc1);
+
+            GridViewColumn gvc2 = new GridViewColumn();
+            gvc2.DisplayMemberBinding = new Binding("LastName");
+            gvc2.Header = "Last Name";
+            gvc2.Width = 100;
+            myGridView.Columns.Add(gvc2);
+
+            myListView.View = myGridView;
+            dockPanel.Children.Add(myListView);
+            myListView.ItemsSource = GetList();
+            myListView.SelectionChanged += MyListView_SelectionChanged;
+        }
+
+        private void MyListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListView.SelectedItem != null)
+            {
+                editButton.IsEnabled = true;
+                deleteButton.IsEnabled = true;
+            }
+        }
+
+        public AuthorsList GetList()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "NIKITOS";
+            builder.InitialCatalog = "Library";
+            builder.IntegratedSecurity = true;
+
+            AuthorsList authors = new AuthorsList();
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Opened");               
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM Authors";
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    authors.Add(new Author((String)reader["FirstName"], (String)reader["LastName"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ooops", MessageBoxButton.OK);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            return authors;
+        }
+
+        public class Author
+        {
+            private String _firstName;
+            private String _lastName;
+
+            public Author(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public string FirstName { get => _firstName; set => _firstName = value; }
+            public string LastName { get => _lastName; set => _lastName = value; }
+        }
+
+        public class AuthorsList : ObservableCollection<Author>
+        {
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "addButton":
+                    AddAuthor();
+                    break;
+                case "editButton":
+                    EditAuthor();
+                    break;
+                case "deleteButton":
+                    DeleteAuthor();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void AddAuthor()
+        {
+            additionalWindow window = new additionalWindow();
+            bool? res = window.ShowDialog();
+
+            if (res != null && res == true)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "NIKITOS";
+                builder.InitialCatalog = "Library";
+                builder.IntegratedSecurity = true;
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Opened");
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO Authors(FirstName, LastName) VALUES (@FirstName, @LastName)";
+
+                    SqlParameter firstNameParam = command.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar, 15);
+
+                    SqlParameter lastNameParam = command.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar, 25);
+
+                    firstNameParam.Value = window.Author.FirstName;
+                    lastNameParam.Value = window.Author.LastName;
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ooops", MessageBoxButton.OK);
+                }
+                finally
+                {
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            myListView.ItemsSource = GetList();
+        }
+
+        public void EditAuthor()
+        {
+            Author oldAuthor = new Author(GetSelecetedAuthor().FirstName, GetSelecetedAuthor().LastName);
+            additionalWindow window = new additionalWindow(GetSelecetedAuthor());
+            bool? res = window.ShowDialog();
+
+            if (res != null && res == true)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "NIKITOS";
+                builder.InitialCatalog = "Library";
+                builder.IntegratedSecurity = true;                
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Opened");
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "UPDATE Authors SET FirstName = @newFirstName, LastName = @newLastName WHERE FirstName = @FirstName AND LastName = @LastName";
+
+                    SqlParameter firstNameParam = command.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar, 15);
+
+                    SqlParameter lastNameParam = command.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar, 25);
+
+                    SqlParameter newFirstNameParam = command.Parameters.Add("@newFirstName", System.Data.SqlDbType.NVarChar, 15);
+
+                    SqlParameter newLastNameParam = command.Parameters.Add("@newLastName", System.Data.SqlDbType.NVarChar, 25);
+
+                    firstNameParam.Value = oldAuthor.FirstName;
+                    lastNameParam.Value = oldAuthor.LastName;
+
+                    newFirstNameParam.Value = window.Author.FirstName;
+                    newLastNameParam.Value = window.Author.LastName;
+
+                    command.ExecuteNonQuery();                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ooops", MessageBoxButton.OK);
+                }
+                finally
+                {
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            myListView.ItemsSource = GetList();
+        }
+
+        public void DeleteAuthor()
+        {
+            MessageBoxResult res = MessageBox.Show("Are u sure?", "Really?", MessageBoxButton.YesNo);
+            if (res == MessageBoxResult.Yes)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "NIKITOS";
+                builder.InitialCatalog = "Library";
+                builder.IntegratedSecurity = true;
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Opened");
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "DELETE FROM Authors WHERE FirstName = @FirstName AND LastName = @LastName";
+
+                    SqlParameter firstNameParam = command.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar, 15);
+
+                    SqlParameter lastNameParam = command.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar, 25);
+                    
+                    firstNameParam.Value = GetSelecetedAuthor().FirstName;
+                    lastNameParam.Value = GetSelecetedAuthor().LastName;
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ooops", MessageBoxButton.OK);
+                }
+                finally
+                {
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            myListView.ItemsSource = GetList();
+        }
+
+        public Author GetSelecetedAuthor()
+        {
+            Author author = (Author)myListView.SelectedItem;
+            return author;
         }
     }
 }
---
+++ ListAuthorsADONetWPF/additionalWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static ListAuthorsADONetWPF.MainWindow;
+
+namespace ListAuthorsADONetWPF
+{
+    /// <summary>
+    /// Interaction logic for additionalWindow.xaml
+    /// </summary>
+    public partial class additionalWindow : Window
+    {
+        private Author _author;
+        public additionalWindow()
+        {
+            InitializeComponent();
+        }
+
+        public additionalWindow(Author author)
+        {            
+            InitializeComponent();
+            Author = author;
+        }
+
+        public Author Author { get => _author;
+            set
+            {
+                _author = value;
+                firstNameTB.Text = _author.FirstName;
+                lastNameTB.Text = _author.LastName;
+            }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "okButton":
+                    PressedOKButton();
+                    break;
+                case "cancelButton":
+                    PressedCancelButton();
+                    break;               
+                default:
+                    break;
+            }
+        }
+
+        private void PressedOKButton()
+        {            
+            if (firstNameTB.Text.Count() > 0 && lastNameTB.Text.Count() > 0)
+            {
+                if (_author == null)
+                {
+                    _author = new Author(firstNameTB.Text, lastNameTB.Text);
+                }
+                else
+                {
+                    _author.FirstName = firstNameTB.Text;
+                    _author.LastName = lastNameTB.Text;
+                }
+                DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please, fill all fields", "Ooops", MessageBoxButton.OK);
+            }
+        }
+
+        private void PressedCancelButton()
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
